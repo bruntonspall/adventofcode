109bc295da95d51486738727a1beaf534f022b4b
Michael Brunton-Spall <michael@brunton-spall.co.uk>
2019-11-28 19:57:14 +0000

    Tidy up the parse tests

--- pkg/parse_test.go
+++ pkg/parse_test.go
@@ -9,25 +9,17 @@ import (
 func TestParse(t *testing.T) {
 	cases := []struct {
 		in  string
+		sep string
 		out []int
 	}{
-		{`1
-2
-3
-4`, []int{1, 2, 3, 4}},
-		{`+1
-+2
-+3
-+4`, []int{1, 2, 3, 4}},
-		{"-1\n-2\n-3\n-4", []int{-1, -2, -3, -4}},
-		{`1
-2
-3
-4`, []int{1, 2, 3, 4}},
+		{`1, 2, 3, 4`, ",", []int{1, 2, 3, 4}},
+		{`+1, +2, +3, +4`, ",", []int{1, 2, 3, 4}},
+		{"-1\n-2\n-3\n-4", "\n", []int{-1, -2, -3, -4}},
+		{"1\n2\n3\n4", "\n", []int{1, 2, 3, 4}},
 	}
 	for _, tt := range cases {
 		t.Run(tt.in, func(t *testing.T) {
-			s := Parse(tt.in, "\n")
+			s := Parse(tt.in, tt.sep)
 			if !cmp.Equal(s, tt.out) {
 				t.Errorf("got %q, want %q", s, tt.out)
 			}
