1e41f6d8e864720c83827a718c3208f26fc5f98e
Michael Brunton-Spall <michael@brunton-spall.co.uk>
2019-12-05 17:24:46 +1100

    Day 5 part 1

---
+++ 2019/day5/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+
+	"github.com/bruntonspall/adventofcode/pkg"
+)
+
+// returns part1 and part2
+func run(input string) (part1 string, part2 string) {
+	memory := make([]int, 0)
+	for _, code := range strings.Split(input, ",") {
+		icode, _ := strconv.Atoi(code)
+		memory = append(memory, icode)
+	}
+	var icc pkg.IntCodeComputer
+	icc.Initialise(memory)
+	icc.Input = []int{1}
+	icc.Run()
+	part1 = fmt.Sprintf("%v", icc.Output)
+	// Parse input and return output
+	part2 = ""
+	return
+}
+
+func main() {
+	pkg.Execute(run, nil, puzzle, true)
+}
