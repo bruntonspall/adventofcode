b8bd31fb4ab0cd85bc78b8ad330db6b5725f4e72
Michael Brunton-Spall <michael@brunton-spall.co.uk>
2019-12-09 14:59:53 +1000

    Restore the intcode tests

--- pkg/intcode_test.go
+++ pkg/intcode_test.go
@@ -7,66 +7,60 @@ import (
 	"github.com/google/go-cmp/cmp"
 )
 
-/** With the input and output via channels, this internal inspecting is much harder
+/* With the input and output via channels, this internal inspecting is much harder */
 
+/* Verify that programs run until 99, changing memory as a side effect */
 func TestRunProgram(t *testing.T) {
 	cases := []struct {
-		in  IntCodeComputer
-		out IntCodeComputer
+		program []int
+		out     []int
+		pc      int
 	}{
-		{IntCodeComputer{[]int{99}, 0, nil, nil}, IntCodeComputer{[]int{99}, 0, nil, nil}},
-		{IntCodeComputer{[]int{1, 9, 10, 3, 2, 3, 11, 0, 99, 30, 40, 50}, 0, nil, nil}, IntCodeComputer{[]int{3500, 9, 10, 70, 2, 3, 11, 0, 99, 30, 40, 50}, 8, nil, nil}},
-		{IntCodeComputer{[]int{1, 1, 1, 4, 99, 5, 6, 0, 99}, 0, nil, nil}, IntCodeComputer{[]int{30, 1, 1, 4, 2, 5, 6, 0, 99}, 8, nil, nil}},
-		{IntCodeComputer{[]int{1101, 100, -1, 4, 0}, 0, nil, nil}, IntCodeComputer{[]int{1101, 100, -1, 4, 99}, 4, nil, nil}},
+		{[]int{99}, []int{99}, 0},
+		{[]int{1, 9, 10, 3, 2, 3, 11, 0, 99, 30, 40, 50},
+			[]int{3500, 9, 10, 70, 2, 3, 11, 0, 99, 30, 40, 50}, 8},
+		{[]int{1, 1, 1, 4, 99, 5, 6, 0, 99},
+			[]int{30, 1, 1, 4, 2, 5, 6, 0, 99}, 8},
+		{[]int{1101, 100, -1, 4, 0},
+			[]int{1101, 100, -1, 4, 99}, 4},
 	}
 	for _, tt := range cases {
-		t.Run(fmt.Sprintf("%v", tt.in), func(t *testing.T) {
-			tt.in.Run()
-			if !cmp.Equal(tt.in, tt.out) {
-				t.Errorf("got %v, want %v", tt.in, tt.out)
-			}
-		})
-	}
-}
-func TestAdvance(t *testing.T) {
-	cases := []struct {
-		in  IntCodeComputer
-		out IntCodeComputer
-	}{
-		{IntCodeComputer{[]int{99}, 0, nil, nil}, IntCodeComputer{[]int{99}, 1, nil, nil}},
-		{IntCodeComputer{[]int{1, 9, 10, 3, 2, 3, 11, 0, 99, 30, 40, 50}, 0, nil, nil}, IntCodeComputer{[]int{1, 9, 10, 70, 2, 3, 11, 0, 99, 30, 40, 50}, 4, nil, nil}},
-		{IntCodeComputer{[]int{1, 9, 10, 70, 2, 3, 11, 0, 99, 30, 40, 50}, 4, nil, nil}, IntCodeComputer{[]int{3500, 9, 10, 70, 2, 3, 11, 0, 99, 30, 40, 50}, 8, nil, nil}},
-		{IntCodeComputer{[]int{1, 0, 0, 0, 99}, 0, nil, nil}, IntCodeComputer{[]int{2, 0, 0, 0, 99}, 4, nil, nil}},
-		{IntCodeComputer{[]int{2, 3, 0, 3, 99}, 0, nil, nil}, IntCodeComputer{[]int{2, 3, 0, 6, 99}, 4, nil, nil}},
-		{IntCodeComputer{[]int{2, 4, 4, 5, 99, 0}, 0, nil, nil}, IntCodeComputer{[]int{2, 4, 4, 5, 99, 9801}, 4, nil, nil}},
-		{IntCodeComputer{[]int{1, 1, 1, 4, 99, 5, 6, 0, 99}, 0, nil, nil}, IntCodeComputer{[]int{1, 1, 1, 4, 2, 5, 6, 0, 99}, 4, nil, nil}},
-		{IntCodeComputer{[]int{1, 1, 1, 4, 2, 5, 6, 0, 99}, 4, nil, nil}, IntCodeComputer{[]int{30, 1, 1, 4, 2, 5, 6, 0, 99}, 8, nil, nil}},
-	}
-	for _, tt := range cases {
-		t.Run(fmt.Sprintf("%v", tt.in), func(t *testing.T) {
-			tt.in.Advance()
-			if !cmp.Equal(tt.in, tt.out) {
-				t.Errorf("got %v, want %v", tt.in, tt.out)
+		t.Run(fmt.Sprintf("%v", tt.program), func(t *testing.T) {
+			cpu := NewIntCodeComputer(tt.program)
+			cpu.Run()
+			_, ok := <-cpu.Output
+			if !ok {
+				if !cmp.Equal(cpu.Memory, tt.out) || !cmp.Equal(cpu.PC, tt.pc) {
+					t.Errorf("got %v, want %v", cpu.Memory, tt.out)
+				}
 			}
 		})
 	}
 }
 
-func TestInitialise(t *testing.T) {
+func TestAdvance(t *testing.T) {
 	cases := []struct {
-		in  []int
-		out IntCodeComputer
+		program []int
+		startpc int
+		out     []int
+		pc      int
 	}{
-		{[]int{99}, IntCodeComputer{[]int{99}, 0, nil, nil}},
-		{[]int{1, 9, 10, 3, 2, 3, 11, 0, 99, 30, 40, 50}, IntCodeComputer{[]int{1, 9, 10, 3, 2, 3, 11, 0, 99, 30, 40, 50}, 0, nil, nil}},
-		{[]int{1, 1, 1, 4, 99, 5, 6, 0, 99}, IntCodeComputer{[]int{1, 1, 1, 4, 99, 5, 6, 0, 99}, 0, nil, nil}},
+		{[]int{99}, 0, []int{99}, 0},
+		{[]int{1, 9, 10, 3, 2, 3, 11, 0, 99, 30, 40, 50}, 0, []int{1, 9, 10, 70, 2, 3, 11, 0, 99, 30, 40, 50}, 4},
+		{[]int{1, 9, 10, 70, 2, 3, 11, 0, 99, 30, 40, 50}, 4, []int{3500, 9, 10, 70, 2, 3, 11, 0, 99, 30, 40, 50}, 8},
+		{[]int{1, 0, 0, 0, 99}, 0, []int{2, 0, 0, 0, 99}, 4},
+		{[]int{2, 3, 0, 3, 99}, 0, []int{2, 3, 0, 6, 99}, 4},
+		{[]int{2, 4, 4, 5, 99, 0}, 0, []int{2, 4, 4, 5, 99, 9801}, 4},
+		{[]int{1, 1, 1, 4, 99, 5, 6, 0, 99}, 0, []int{1, 1, 1, 4, 2, 5, 6, 0, 99}, 4},
+		{[]int{1, 1, 1, 4, 2, 5, 6, 0, 99}, 4, []int{30, 1, 1, 4, 2, 5, 6, 0, 99}, 8},
 	}
 	for _, tt := range cases {
-		t.Run(fmt.Sprintf("%v", tt.in), func(t *testing.T) {
-			var icc IntCodeComputer
-			icc.Initialise(tt.in)
-			if !cmp.Equal(icc, tt.out) {
-				t.Errorf("got %v, want %v", icc, tt.out)
+		t.Run(fmt.Sprintf("%v", tt.program), func(t *testing.T) {
+			cpu := NewIntCodeComputer(tt.program)
+			cpu.PC = tt.startpc
+			cpu.Advance()
+			if !cmp.Equal(cpu.Memory, tt.out) || !cmp.Equal(cpu.PC, tt.pc) {
+				t.Errorf("got %v(%v), want %v(%v)", cpu.Memory, cpu.PC, tt.out, tt.pc)
 			}
 		})
 	}
@@ -74,28 +68,28 @@ func TestInitialise(t *testing.T) {
 
 func TestParameterModes(t *testing.T) {
 	cases := []struct {
-		in  IntCodeComputer
-		out IntCodeComputer
+		in  []int
+		out []int
 	}{
-		{IntCodeComputer{[]int{1, 5, 6, 7, 99, 20, 30, 0}, 0, nil, nil}, IntCodeComputer{[]int{1, 5, 6, 7, 99, 20, 30, 50}, 4, nil, nil}},
-		{IntCodeComputer{[]int{1101, 5, 6, 7, 99, 20, 30, 0}, 0, nil, nil}, IntCodeComputer{[]int{1101, 5, 6, 7, 99, 20, 30, 11}, 4, nil, nil}},
-		{IntCodeComputer{[]int{1001, 5, 6, 7, 99, 20, 30, 0}, 0, nil, nil}, IntCodeComputer{[]int{1001, 5, 6, 7, 99, 20, 30, 26}, 4, nil, nil}},
-		{IntCodeComputer{[]int{101, 5, 6, 7, 99, 20, 30, 0}, 0, nil, nil}, IntCodeComputer{[]int{101, 5, 6, 7, 99, 20, 30, 35}, 4, nil, nil}},
-		{IntCodeComputer{[]int{2, 5, 6, 7, 99, 20, 30, 0}, 0, nil, nil}, IntCodeComputer{[]int{2, 5, 6, 7, 99, 20, 30, 600}, 4, nil, nil}},
-		{IntCodeComputer{[]int{1102, 5, 6, 7, 99, 20, 30, 0}, 0, nil, nil}, IntCodeComputer{[]int{1102, 5, 6, 7, 99, 20, 30, 30}, 4, nil, nil}},
-		{IntCodeComputer{[]int{1002, 5, 6, 7, 99, 20, 30, 0}, 0, nil, nil}, IntCodeComputer{[]int{1002, 5, 6, 7, 99, 20, 30, 120}, 4, nil, nil}},
-		{IntCodeComputer{[]int{102, 5, 6, 7, 99, 20, 30, 0}, 0, nil, nil}, IntCodeComputer{[]int{102, 5, 6, 7, 99, 20, 30, 150}, 4, nil, nil}},
+		{[]int{1, 5, 6, 7, 99, 20, 30, 0}, []int{1, 5, 6, 7, 99, 20, 30, 50}},
+		{[]int{1101, 5, 6, 7, 99, 20, 30, 0}, []int{1101, 5, 6, 7, 99, 20, 30, 11}},
+		{[]int{1001, 5, 6, 7, 99, 20, 30, 0}, []int{1001, 5, 6, 7, 99, 20, 30, 26}},
+		{[]int{101, 5, 6, 7, 99, 20, 30, 0}, []int{101, 5, 6, 7, 99, 20, 30, 35}},
+		{[]int{2, 5, 6, 7, 99, 20, 30, 0}, []int{2, 5, 6, 7, 99, 20, 30, 600}},
+		{[]int{1102, 5, 6, 7, 99, 20, 30, 0}, []int{1102, 5, 6, 7, 99, 20, 30, 30}},
+		{[]int{1002, 5, 6, 7, 99, 20, 30, 0}, []int{1002, 5, 6, 7, 99, 20, 30, 120}},
+		{[]int{102, 5, 6, 7, 99, 20, 30, 0}, []int{102, 5, 6, 7, 99, 20, 30, 150}},
 	}
 	for _, tt := range cases {
 		t.Run(fmt.Sprintf("%v", tt.in), func(t *testing.T) {
-			tt.in.Advance()
-			if !cmp.Equal(tt.in, tt.out) {
-				t.Errorf("got %v, want %v", tt.in, tt.out)
+			cpu := NewIntCodeComputer(tt.in)
+			cpu.Advance()
+			if !cmp.Equal(cpu.Memory, tt.out) {
+				t.Errorf("got %v, want %v", cpu.Memory, tt.out)
 			}
 		})
 	}
 }
-*/
 
 func TestIOInstructions(t *testing.T) {
 	cases := []struct {
