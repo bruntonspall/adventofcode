802842adbdf578a6ae84349d8bdbf7b9ea250ee5
Michael Brunton-Spall <michael@brunton-spall.co.uk>
2020-12-01 20:54:23 +0000

    Try using itertools instead of manually going through the lists

--- 2020/day1/puzzle.py
+++ 2020/day1/puzzle.py
@@ -1,14 +1,13 @@
+import itertools
+
 def find_pair(l, target):
-    for a in l:
-        for b in l:
-            if a != b and a+b == target:
-                return (a,b)
+    for (a,b) in itertools.combinations(l,2):
+        if a+b == target:
+            return (a,b)
     return (0,0)
 
 def find_triple(l, target):
-    for a in l:
-        for b in l:
-            for c in l:
-                if a != b and a != c and b != c and a+b+c == target:
-                    return (a,b,c)
+    for (a,b,c) in itertools.combinations(l,3):
+        if a+b+c == target:
+            return (a,b,c)
     return (0,0,0)
