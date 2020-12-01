28260f86a65955b3500887975e59debf52277c70
Michael Brunton-Spall <michael@brunton-spall.co.uk>
2020-12-01 20:39:35 +0000

    Extend find double into find triple

--- 2020/day1/puzzle.py
+++ 2020/day1/puzzle.py
@@ -4,3 +4,11 @@ def find_pair(l, target):
             if a != b and a+b == target:
                 return (a,b)
     return (0,0)
+
+def find_triple(l, target):
+    for a in l:
+        for b in l:
+            for c in l:
+                if a != b and a != c and b != c and a+b+c == target:
+                    return (a,b,c)
+    return (0,0,0)
--- 2020/day1/puzzle_test.py
+++ 2020/day1/puzzle_test.py
@@ -9,12 +9,29 @@ from utils import listofnumbers
        ([1721,979,366,299,675,1456], 2020, (1721,299))
     ],
 )
-def test_calculate_score(l, target, pair):
+def test_find_pair(l, target, pair):
     actual = puzzle.find_pair(l, target)
     assert pair == actual
 
-def test_final():
+@pytest.mark.parametrize(
+    "l,target,triple",
+    [
+        ([1,2,3,4,5,6], 9, (1,2,6)),
+       ([1721,979,366,299,675,1456], 2020, (979, 366, 675))
+    ],
+)
+def test_find_triple(l, target, triple):
+    actual = puzzle.find_triple(l, target)
+    assert triple == actual
+
+def test_part1():
     data = listofnumbers(open("input.txt").readlines())
     actual = puzzle.find_pair(data, 2020)
     print(actual[0]*actual[1])
     assert (1073,947) == actual
+
+def test_part2():
+    data = listofnumbers(open("input.txt").readlines())
+    actual = puzzle.find_triple(data, 2020)
+    print(actual[0]*actual[1]*actual[2])
+    assert (911, 618, 491) == actual
