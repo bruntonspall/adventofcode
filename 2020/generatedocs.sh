67bd8662fba2683b89bffb4eca6f4e839172a0f2
Michael Brunton-Spall <michael@brunton-spall.co.uk>
2020-12-12 13:50:49 +0000

    Update docs

--- 2020/generatedocs.sh
+++ 2020/generatedocs.sh
@@ -8,3 +8,14 @@ echo ${days/ipynb/html}
 		jupyter nbconvert --to pdf $days
 	fi
 done
+
+cat > index.md <<EOF
+# Advent of Code 2020
+This is my set of entries for [advent of code 2020](https://www.adventofcode.com/2020).
+
+## Completed days
+  So far I've completed the following days:
+EOF
+for days in *.html; do
+	echo "[${days/.html/}]($days)" >> index.md
+done;
