1b45eca02d5d468071a91f342785169987d595d6
Michael Brunton-Spall <michael@brunton-spall.co.uk>
2024-11-29 18:29:49 +0000

    Run rustfmt to tidy up code

--- rust-2021/src/day1.rs
+++ rust-2021/src/day1.rs
@@ -1,22 +1,17 @@
 #[aoc_generator(day1)]
 pub fn input_generator(input: &str) -> Vec<u32> {
-    input
-        .lines()
-        .map(|l| { 
-            l.parse::<u32>().unwrap()
-        }).collect()
+    input.lines().map(|l| l.parse::<u32>().unwrap()).collect()
 }
-
 
 #[aoc(day1, part1, u32)]
 pub fn part1(input: &Vec<u32>) -> usize {
-    input.windows(2).filter(|i|i[0] < i[1]).count()
+    input.windows(2).filter(|i| i[0] < i[1]).count()
 }
 
 #[aoc(day1, part2, u32)]
 pub fn part2(input: &Vec<u32>) -> usize {
-    let windows: Vec<u32> = input.windows(3).map(|w|w.iter().sum()).collect();
-    windows.windows(2).filter(|i|i[0] < i[1]).count()
+    let windows: Vec<u32> = input.windows(3).map(|w| w.iter().sum()).collect();
+    windows.windows(2).filter(|i| i[0] < i[1]).count()
 }
 
 #[cfg(test)]
@@ -25,13 +20,13 @@ mod tests {
 
     #[test]
     fn find_depths() {
-        let input = vec![199,200,208,210,200,207,240,269,260,263];
+        let input = vec![199, 200, 208, 210, 200, 207, 240, 269, 260, 263];
         assert_eq!(part1(&input), 7);
     }
 
     #[test]
     fn compare_windows() {
-        let input = vec![199,200,208,210,200,207,240,269,260,263];
+        let input = vec![199, 200, 208, 210, 200, 207, 240, 269, 260, 263];
         assert_eq!(part2(&input), 5);
     }
 }
--- rust-2021/src/day2.rs
+++ rust-2021/src/day2.rs
@@ -1,9 +1,8 @@
-
 #[derive(PartialEq, Debug)]
 pub enum Command {
-    Forward (i32),
-    Up (i32),
-    Down (i32),
+    Forward(i32),
+    Up(i32),
+    Down(i32),
 }
 
 pub fn parse_line(input: &str) -> Command {
@@ -14,27 +13,23 @@ pub fn parse_line(input: &str) -> Command {
         Some("forward") => Command::Forward(arg.parse::<i32>().unwrap()),
         Some("up") => Command::Up(arg.parse::<i32>().unwrap()),
         Some("down") => Command::Down(arg.parse::<i32>().unwrap()),
-        _ => panic!("This shouldn't happen")
+        _ => panic!("This shouldn't happen"),
     }
-
 }
 
 #[aoc_generator(day2)]
 pub fn input_generator(input: &str) -> Vec<Command> {
-    input
-        .lines()
-        .map(parse_line).collect()
+    input.lines().map(parse_line).collect()
 }
-
 
 #[aoc(day2, part1, i32)]
 pub fn part1(input: &Vec<Command>) -> i32 {
     use Command::*;
 
-    let total = input.iter().fold((0,0), |acc, command| match command {
-        Forward(x) => (acc.0 + x,acc.1),
+    let total = input.iter().fold((0, 0), |acc, command| match command {
+        Forward(x) => (acc.0 + x, acc.1),
         Up(x) => (acc.0, acc.1 - x),
-        Down(x) => (acc.0, acc.1 + x)
+        Down(x) => (acc.0, acc.1 + x),
     });
     total.0 * total.1
 }
@@ -43,18 +38,18 @@ pub fn part1(input: &Vec<Command>) -> i32 {
 pub fn part2(input: &Vec<Command>) -> i32 {
     use Command::*;
 
-    let total = input.iter().fold((0,0,0), |acc, command| match command {
-        Forward(x) => (acc.0 + x,acc.1+x*acc.2, acc.2),
-        Up(x) => (acc.0, acc.1, acc.2-x),
-        Down(x) => (acc.0, acc.1, acc.2+x)
+    let total = input.iter().fold((0, 0, 0), |acc, command| match command {
+        Forward(x) => (acc.0 + x, acc.1 + x * acc.2, acc.2),
+        Up(x) => (acc.0, acc.1, acc.2 - x),
+        Down(x) => (acc.0, acc.1, acc.2 + x),
     });
     total.0 * total.1
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::day2::{parse_line, part1, part2};
     use crate::day2::Command::*;
+    use crate::day2::{parse_line, part1, part2};
 
     #[test]
     fn test_parse() {
