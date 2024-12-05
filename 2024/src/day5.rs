d9d850330e8a219caa74be0d0cff854c08e7a6a7
Michael Brunton-Spall <michael@brunton-spall.co.uk>
2024-12-05 07:25:07 +0000

    Parse input format
    This is a bit horrible.  Particuarly unhappy with the split"|" match combo.  I feel like I should be able to deconstruct better here for a cleaner, better match that is safer.

--- 2024/src/day5.rs
+++ 2024/src/day5.rs
@@ -1,13 +1,43 @@
 /*
 * day5, Part 1.
+* Right, read a list of predicates, followed by some input data, and check all the predicates match... simples!
+* Reading the text carefully, we can probably simplify this all down.  I was originally considering creating
+* curried functions, and building a list of them, and then applying them to the input data.
+* But actually in this case, the test is always the same, for each number, find all the rules with the start number
+* and then process all the successor numbers against the rules.  If there's an error, break out and invalidate the row.
+* We can hardcode the processing itself, so the data structure can almost certainly just be tuples for the pairs, and
+* then a vector for each update.
 */
 
-type GeneratorResult = Vec<u32>;
+type GeneratorResult = (Vec<(u32, u32)>, Vec<Vec<u32>>);
 type RunResult = u32;
 
 #[aoc_generator(day5)]
 pub fn input_generator(input: &str) -> GeneratorResult {
-    todo!();
+    // Right parsing, we're going to parse lines until an empty line, and then we parse the results separately.
+
+    let left: Vec<(u32, u32)> = input
+        .lines()
+        .take_while(|line| line != &"")
+        .map(|line| match line.split("|").collect::<Vec<&str>>() {
+            pair => (
+                pair[0].parse::<u32>().expect("Numbers only"),
+                pair[1].parse::<u32>().expect("Numbers only"),
+            ),
+            _ => (0, 0),
+        })
+        .collect();
+    let right = input
+        .lines()
+        .skip_while(|line| line != &"")
+        .skip(1)
+        .map(|line| {
+            line.split(',')
+                .map(|num| num.parse::<u32>().expect("Numbers only"))
+                .collect()
+        })
+        .collect();
+    (left, right)
 }
 
 #[aoc(day5, part1, RunResult)]
@@ -30,7 +60,19 @@ mod tests {
     use crate::day5::*;
 
     #[test]
-    fn test_generator() {}
+    fn test_generator() {
+        let input = "47|53
+97|13
+97|61
+
+75,47,61,53,29
+75,29,13";
+        let expected = (
+            vec![(47, 53), (97, 13), (97, 61)],
+            vec![vec![75, 47, 61, 53, 29], vec![75, 29, 13]],
+        );
+        assert_eq!(expected, input_generator(input));
+    }
 
     #[test]
     fn test_part1() {
