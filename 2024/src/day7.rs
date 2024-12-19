91661b24e59be4747163a1adb3535f5619d6d5b7
Michael Brunton-Spall <michael@brunton-spall.co.uk>
2024-12-19 19:22:29 +0000

    Convert to use strings instead of numbers

--- 2024/src/day7.rs
+++ 2024/src/day7.rs
@@ -5,7 +5,7 @@
 #[derive(Debug, Clone, PartialEq, Hash, Eq)]
 pub struct Equation {
     total: u64,
-    args: Vec<u64>,
+    args: Vec<String>,
 }
 
 pub enum Operand {
@@ -16,17 +16,17 @@ pub enum Operand {
 type GeneratorResult = Vec<Equation>;
 type RunResult = usize;
 
-pub fn total(target: u64, args: &Vec<u64>) -> u32 {
-    pub fn total_rec(target: u64, running_total: u64, args: &Vec<u64>, index: usize) -> u32 {
+pub fn total(target: u64, args: &Vec<String>) -> u32 {
+    pub fn total_rec(target: u64, running_total: u64, args: &Vec<String>, index: usize) -> u32 {
         // Our base case, make sure it returns when we run out of numbers
         if index == args.len() {
             return if running_total == target { 1 } else { 0 };
         }
         // Handle addition, which is running_total + args[index] and repeat for next index
-        total_rec(target, running_total + args[index], args, index + 1)
-            + total_rec(target, running_total * args[index], args, index + 1)
+        total_rec(target, running_total + args[index].parse::<u64>().expect("args must be a number"), args, index + 1)
+            + total_rec(target, running_total * args[index].parse::<u64>().expect("args must be a number"), args, index + 1)
     }
-    total_rec(target, args[0], args, 1)
+    total_rec(target, args[0].parse::<u64>().expect("args must be a number"), args, 1)
 }
 
 #[aoc_generator(day7)]
@@ -46,7 +46,7 @@ pub fn input_generator(input: &str) -> GeneratorResult {
                 .next()
                 .unwrap()
                 .split_whitespace()
-                .map(|num| num.parse::<u64>().expect("Must be numbers"))
+                .map(|num| String::from(num))
                 .collect();
             Equation {
                 total: sum_s as u64,
@@ -89,7 +89,7 @@ mod tests {
         assert_eq!(
             vec![Equation {
                 total: 7,
-                args: vec![1, 2, 3]
+                args: vec![1.to_string(), 2.to_string(), 3.to_string()]
             }],
             input_generator("7: 1 2 3")
         );
@@ -97,11 +97,11 @@ mod tests {
             vec![
                 Equation {
                     total: 190,
-                    args: vec![10, 19]
+                    args: vec![10.to_string(), 19.to_string()]
                 },
                 Equation {
                     total: 3267,
-                    args: vec![81, 40, 27]
+                    args: vec![81.to_string(), 40.to_string(), 27.to_string()]
                 },
             ],
             input_generator(
@@ -115,21 +115,23 @@ mod tests {
     fn test_total() {
         // We're going to change these tests when we add multiplication
         // Check base case, in this case we should reach 5
-        assert_eq!(1, total(5, &vec![5]));
+        assert_eq!(1, total(5, &vec!["5".to_string()]));
 
         // 1+2 and 1*2
-        assert_eq!(1, total(2, &vec![1, 2]));
+        assert_eq!(1, total(2, &vec!["1".to_string(), "2".to_string()]));
 
         // 1+2+3=6, 1+2*3=9, 1*2+3=5, 1*2*3=6
-        assert_eq!(2, total(6, &vec![1, 2, 3]));
-        assert_eq!(1, total(5, &vec![1, 2, 3]));
-        assert_eq!(1, total(9, &vec![1, 2, 3]));
+        let args = vec![1.to_string(), 2.to_string(), 3.to_string()];
+        assert_eq!(2, total(6, &args));
+        assert_eq!(1, total(5, &args));
+        assert_eq!(1, total(9, &args));
 
         // 1+2+4=7, 1+2*4=12, 1*2+4=6, 1*2*4=8
-        assert_eq!(1, total(6, &vec![1, 2, 4]));
-        assert_eq!(1, total(7, &vec![1, 2, 4]));
-        assert_eq!(1, total(8, &vec![1, 2, 4]));
-        assert_eq!(1, total(12, &vec![1, 2, 4]));
+        let args = vec![1.to_string(), 2.to_string(), 4.to_string()];
+        assert_eq!(1, total(6, &args));
+        assert_eq!(1, total(7, &args));
+        assert_eq!(1, total(8, &args));
+        assert_eq!(1, total(12, &args));
     }
 
     #[test]
